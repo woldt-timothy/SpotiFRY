518444d5f181f7ea5a1f4cab098ab335de7988ae
Woldt, Timothy A. (Student) <300028034@fvtc.edu>
2018-12-03 14:30:40 -0600

    Added some BL stuff

--- TWDP.PlayList/TWDP.Playlist.BL/User.cs
+++ TWDP.PlayList/TWDP.Playlist.BL/User.cs
@@ -18,18 +18,30 @@ namespace TWDP.Playlist.BL
 
 
 
-        //May or May not use in our appp
+        //May or May not use in our appp //tim -- you need to hash password
         public void Insert()
         {
             try
             {
                 using (playlistEntities dc = new playlistEntities())
                 {
+                    tblUser user = new tblUser();
+                    user.UserId = Guid.NewGuid();
+                    user.Email = Email;
+                    user.FirstName = FirstName;
+                    user.LastName = LastName;
+                    user.LoginId = LoginId;
+                    user.Password = Password;
+
+                    dc.tblUsers.Add(user);
+                    dc.SaveChanges();
 
                 }
             }
             catch (Exception e)
             {
+
+           
 
                 throw e;
             }
@@ -41,6 +53,18 @@ namespace TWDP.Playlist.BL
         {
             try
             {
+                using (playlistEntities dc = new playlistEntities())
+                {
+                    tblUser user = dc.tblUsers.Where(u => u.UserId == UserId).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        dc.tblUsers.Remove(user);
+                        dc.SaveChanges();
+                    }
+
+
+                }
 
             }
             catch (Exception e)
@@ -54,12 +78,59 @@ namespace TWDP.Playlist.BL
         }
 
 
+        
+
         //Take a look at this again //May or May not use in our appp
         public void Update()
         {
             try
             {
+                using (playlistEntities dc = new playlistEntities())
+                {
+                    tblUser user = dc.tblUsers.FirstOrDefault(u => u.UserId == UserId);
+                    if (user != null)
+                    {
+                        user.Email = Email;
+                        user.FirstName = FirstName;
+                        user.LastName = LastName;
+                        user.LoginId = LoginId;
+                        user.Password = Password;
+                        dc.SaveChanges();
+                    }
 
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public void LoadById(Guid guid)
+        {
+            try
+            {
+                using (playlistEntities dc = new playlistEntities())
+                {
+
+                    var user = dc.tblUsers.FirstOrDefault(u => u.UserId == guid);
+                    if (user != null)
+                    {
+                        UserId = user.UserId;
+                        FirstName = user.FirstName;
+                        LastName = user.LastName;
+                        Email = user.Email;
+                        Password = user.Password;
+                        LoginId = user.LoginId;
+                    }
+
+
+
+
+                    
+                }
+                    
             }
             catch (Exception e)
             {
