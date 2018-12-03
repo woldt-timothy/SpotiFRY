5321858a5d0c9ae0ffe69ec91537cc3f209bffff
Woldt, Timothy A. (Student) <300028034@fvtc.edu>
2018-12-02 20:59:36 -0600

    asda

--- TWDP.PlayList/TWDP.PlayList.PL/tblUser.cs
+++ TWDP.PlayList/TWDP.PlayList.PL/tblUser.cs
@@ -17,5 +17,8 @@ namespace TWDP.PlayList.PL
         public System.Guid UserId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LoginId { get; set; }
     }
 }
