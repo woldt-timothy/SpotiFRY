bf916f2e039e00422620d7aa69080a4df0357c06
Dalton Polomis <300028026@fvtc.edu>
2018-12-03 22:07:02 -0600

    added song.cs and utsong.cs

---
+++ TWDP.PlayList/TWDP.PlayList.BL.Test/utSong.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using TWDP.Playlist.BL;
+using TWDP.PlayList.PL;
+
+
+namespace TWDP.PlayList.BL.Test
+{
+    [TestClass]
+    public class utUser
+    {
+        [TestMethod]
+        public void InsertTest()
+        {
+
+            User user = new User();
+
+            user.Email = "smally@yahoo.com";
+            user.FirstName = "Biggie";
+            user.LastName = "Smalls";
+            user.LoginId = "smallDude";
+            user.Password = "apple";
+            user.Insert();
+
+
+            playlistEntities dc = new playlistEntities();
+
+            var users = dc.tblUsers;
+            int expected = 6;
+            int actual = users.Count();
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+
+            User user = new User();
+
+      
+            user.LoginId = "smallDude";
+      
+            user.Delete();
+
+
+            playlistEntities dc = new playlistEntities();
+
+            var users = dc.tblUsers;
+            int expected = 6;
+            int actual = users.Count();
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+
+            User user = new User();
+
+
+            user.LoginId = "BadBadBoba";
+            user.LastName = "Time Bomb";
+            user.Password = "YoYo";
+            user.Email = "yoyo@gmail.com";
+
+            user.Update();
+
+
+            User otherUser = new User();
+
+
+            otherUser.LoadById("BadBadBoba");
+
+
+
+
+           
+            string expected = "Time Bomb";
+
+            string actual = otherUser.LastName;
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ TWDP.PlayList/TWDP.Playlist.BL/Song.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWDP.PlayList.PL;
+
+namespace TWDP.Playlist.BL
+{
+    public class Song
+    {
+        public Guid SongId { get; set; }
+        public string SongTitle { get; set; }
+        public string Artist { get; set; }
+        public string AlbumTitle { get; set; }
+        public string ImagePath { get; set; }
+        //public List<Songs> { get; set; } **note to tim and dalton will make on tuesday
+
+
+        public bool Insert()
+        {
+            int result = 0;
+            try
+            {
+                using (playlistEntities dc = new playlistEntities())
+                {
+                    tblSuggestedSong suggestedSong = new tblSuggestedSong();
+                    suggestedSong.SuggestedSongId = Guid.NewGuid();
+                    suggestedSong.SuggestedSongTitle = this.SongTitle;
+                    suggestedSong.SuggestedSongArtist = this.Artist;
+                    suggestedSong.SuggestedSongAlbumTitle = this.AlbumTitle;
+                    suggestedSong.SuggestedSongImagePath = this.ImagePath;
+
+                    dc.tblSuggestedSongs.Add(suggestedSong);
+                    result = dc.SaveChanges();
+                }
+
+                return result == 0 ? false : true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw ex;
+            }
+        }
+
+    }
+}
