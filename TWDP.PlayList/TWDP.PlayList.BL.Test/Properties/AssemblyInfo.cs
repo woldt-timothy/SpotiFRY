365b39b185266bffca03f09994cfd90e5c1add3a
Woldt, Timothy A. (Student) <300028034@fvtc.edu>
2018-12-03 18:10:28 -0600

    Initial Push 06:10:00

---
+++ TWDP.PlayList/TWDP.PlayList.BL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TWDP.PlayList.BL.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TWDP.PlayList.BL.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad641809-6ba8-462c-ad81-7b1f1f8847a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TWDP.PlayList/TWDP.PlayList.BL.Test/utUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TWDP.PlayList.BL.Test
+{
+    [TestClass]
+    public class utUser
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
