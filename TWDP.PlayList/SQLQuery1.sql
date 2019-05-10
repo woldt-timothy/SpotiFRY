e79b54598bb12e7a479b11d18f250a84baddc398
Tim Woldt <31869613+woldt-timothy@users.noreply.github.com>
2019-05-10 16:46:42 -0500

    Delete SQLQuery1.sql

--- TWDP.PlayList/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿
-CREATE TABLE [dbo].[ErrorLog] (
-    [ErrDate]    DATETIME       NULL,
-    [ErrThread]  VARCHAR (200)  NULL,
-    [ErrLevel]   VARCHAR (200)  NULL,
-    [ErrLogger]  VARCHAR (200)  NULL,
-    [ErrMessage] VARCHAR (2000) NULL
-);
