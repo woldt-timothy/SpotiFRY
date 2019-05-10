c6991e51c0e16cce929a55cecbb7a198c351ff39
Woldt, Timothy A. (Student) <woldt.timothy@gmail.com>
2019-05-10 15:54:04 -0500

    Clean Up Unit Tests

--- TWDP.PlayList/TWDP.PlayList.BL.Test/utUser.cs
+++ TWDP.PlayList/TWDP.PlayList.BL.Test/utUser.cs
@@ -13,7 +13,6 @@ namespace TWDP.PlayList.BL.Test
         [TestMethod]
         public void InsertTest()
         {
-
             User user = new User();
 
             user.Email = "smally@yahoo.com";
@@ -24,7 +23,6 @@ namespace TWDP.PlayList.BL.Test
             user.SpotifyId = "Lebowski11";
             user.Insert();
 
-
             playlistEntities dc = new playlistEntities();
 
             var users = dc.tblUsers;
@@ -32,48 +30,28 @@ namespace TWDP.PlayList.BL.Test
             int actual = users.Count();
 
             Assert.AreEqual(expected, actual);
-
-
         }
-
 
         [TestMethod]
         public void LoadByLoginIdTest()
         {
-
             User user = new User();
 
-            
-            
             user.LoadById("smallDude");
-
 
             playlistEntities dc = new playlistEntities();
 
-
-
             string expected = "Smalls";
 
-
             Assert.AreEqual(expected, user.LastName);
-
-
         }
-
-
-
 
         [TestMethod]
         public void DeleteTest()
         {
-
             User user = new User();
-
-      
             user.LoginId = "smallDude";
-      
             user.Delete();
-
 
             playlistEntities dc = new playlistEntities();
 
@@ -82,17 +60,13 @@ namespace TWDP.PlayList.BL.Test
             int actual = users.Count();
 
             Assert.AreEqual(expected, actual);
-
-
         }
 
 
         [TestMethod]
         public void UpdateTest()
         {
-
             User user = new User();
-
 
             user.Email = "smally@yahoo.com";
             user.FirstName = "Smally";
@@ -103,50 +77,29 @@ namespace TWDP.PlayList.BL.Test
 
             user.Update();
 
-
             User otherUser = new User();
-
-
             otherUser.LoadById("smallDude");
 
-
-
-
-           
             string expected = "Smally";
-
             string actual = otherUser.FirstName;
 
             Assert.AreEqual(expected, actual);
-
-
         }
 
         [TestMethod]
         public void LoginTest()
         {
             User user = new User();
-            user.LoginId = "woldt";
-            user.Password = "maple";
+            user.LoginId = "LoginId";
+            user.Password = "Password";
             if (user.Login() == true)
             {
                 Assert.AreEqual(true, user.Login());
             }
             else
             {
-
+                return;
             }
-
-
-
         }
-
-
-
-
-
     }
-
-
-
 }
